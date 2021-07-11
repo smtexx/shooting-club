4854f5e89839952aa6e4639b669a9a9bd82d5b7f
Roman Kolmakov <cold.snowww@yahoo.com>
2021-07-11 13:21:40 +0700

    End visual edit _photo-cover block

--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ import './html/_page-data/index/_gallery/_gallery';
 import './html/_common-blocks/_footer/_footer';
 import './html/_page-data/tournaments/_tournament-card/_tournament-card';
 import './html/_page-data/photos/_photo-card/_photo-card';
+import './html/_page-data/photos/_photo-cover/_photo-cover';
