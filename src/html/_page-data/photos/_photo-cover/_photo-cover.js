82f73c65ab81dbfe7549db4f78439621fdf50add
Roman Kolmakov <cold.snowww@yahoo.com>
2021-07-17 12:08:17 +0700

    Ready photo gallery without animation

---
+++ src/html/_page-data/photos/_photo-cover/_photo-cover.js
@@ -0,0 +1,36 @@
+import PhotoGallery from "../../../../common_modules/PhotoGallery";
+
+import slide_1_750 from '../../../../assets/img/gallery/slide-1-750w.jpg'; 
+import slide_1_1024 from '../../../../assets/img/gallery/slide-1-1024w.jpg'; 
+import slide_2_750 from '../../../../assets/img/gallery/slide-2-750w.jpg'; 
+import slide_2_1024 from '../../../../assets/img/gallery/slide-2-1024w.jpg'; 
+import slide_3_750 from '../../../../assets/img/gallery/slide-3-750w.jpg'; 
+import slide_3_1024 from '../../../../assets/img/gallery/slide-3-1024w.jpg'; 
+import slide_4_750 from '../../../../assets/img/gallery/slide-4-750w.jpg'; 
+import slide_4_1024 from '../../../../assets/img/gallery/slide-4-1024w.jpg'; 
+import slide_5_750 from '../../../../assets/img/gallery/slide-5-750w.jpg'; 
+import slide_5_1024 from '../../../../assets/img/gallery/slide-5-1024w.jpg'; 
+import slide_6_750 from '../../../../assets/img/gallery/slide-6-750w.jpg'; 
+import slide_6_1024 from '../../../../assets/img/gallery/slide-6-1024w.jpg'; 
+import slide_7_750 from '../../../../assets/img/gallery/slide-7-750w.jpg'; 
+import slide_7_1024 from '../../../../assets/img/gallery/slide-7-1024w.jpg'; 
+import slide_8_750 from '../../../../assets/img/gallery/slide-8-750w.jpg'; 
+import slide_8_1024 from '../../../../assets/img/gallery/slide-8-1024w.jpg'; 
+import slide_9_750 from '../../../../assets/img/gallery/slide-9-750w.jpg'; 
+import slide_9_1024 from '../../../../assets/img/gallery/slide-9-1024w.jpg'; 
+
+new PhotoGallery({
+    resolutions: [750, 1024], 
+    links: [
+        [slide_1_750, slide_1_1024],
+        [slide_2_750, slide_2_1024],
+        [slide_3_750, slide_3_1024],
+        [slide_4_750, slide_4_1024],
+        [slide_5_750, slide_5_1024],
+        [slide_6_750, slide_6_1024],
+        [slide_7_750, slide_7_1024],
+        [slide_8_750, slide_8_1024],
+        [slide_9_750, slide_9_1024]
+    ]
+});
+
